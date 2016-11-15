12a2841662885b31cec93c1517c3de0f5b7a1ff0
Topaz Grant <topaz.grant@mymona.uwi.edu>
2016-11-15 23:21:33 +0000

    Exercise 4(1,2): create sql query to return all countries or no countries

--- world.php
+++ world.php
@@ -1,5 +1,6 @@
 <?php
 $text = $_GET['country'];
+$check = $_GET['all'];
 
 $host = getenv('IP');
 $username = getenv('C9_USER');
@@ -9,11 +10,23 @@ $dbname = 'world';
 $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
 
 $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$text%'");
+$stmt2 = $conn->query("SELECT *  FROM countries");
 
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+if($check == 'true'){
+    $results = $stmt2->fetchAll(PDO::FETCH_ASSOC);
+}
+else{
+    
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
+
+if(count($results) == 0){
+    echo 'No country';
+}
+else{
 
 echo '<ul>';
 foreach ($results as $row) {
   echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
 }
-echo '</ul>';
+echo '</ul>';}
